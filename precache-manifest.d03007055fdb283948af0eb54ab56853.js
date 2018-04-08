90659980c3eafecc800c304efe16e0d8716e5586
BOSTON\sivaraj.v <zural143@gmail.com>
2018-04-08 13:04:42 +0530

    Checin

--- precache-manifest.d03007055fdb283948af0eb54ab56853.js
+++ precache-manifest.d03007055fdb283948af0eb54ab56853.js
@@ -3,17 +3,16 @@ self.__precacheManifest = [
     "url": "img/icons/icon_144x144.f92f83ad7c13b5bd845cedc3193e3549.png"
   },
   {
+    "url": "main-7b41f79c1f4186b244fd.js"
+  },
+  {
+    "url": "main-aad3cdf77f2176ae0b2e.css"
+  },
+  {
+    "url": "img/icons/icon_120x120.7eadb215beec2ed72e5eac4a51da56fb.png"
+  },
+  {
     "url": "img/icons/icon_16x16.ff8abb3b6b6f7f47ee9de80d1cbc1906.png"
-  },
-  {
-    "revision": "f6d5fba1c21dc80e76a95ec7903c469e",
-    "url": "index.html"
-  },
-  {
-    "url": "img/icons/icon_32x32.62508381cb3f699b4e7c33215c2ca619.png"
-  },
-  {
-    "url": "img/icons/icon_512x512.fe1ca0d9703f62b945afde8e6dd15c7c.png"
   },
   {
     "url": "img/icons/icon_384x384.ee92216ee37b26d592f1e31eb96b9bc9.png"
@@ -28,19 +27,29 @@ self.__precacheManifest = [
     "url": "img/icons/icon_180x180.f89fc0ad2e87ba60741608c38aa7f5b5.png"
   },
   {
+    "url": "img/icons/icon_32x32.62508381cb3f699b4e7c33215c2ca619.png"
+  },
+  {
+    "revision": "d851ab5b524f4220969447bda3e45f42",
+    "url": "index.html"
+  },
+  {
     "url": "img/icons/icon_57x57.980b830a8cd6213ea47b3fac896caa29.png"
+  },
+  {
+    "url": "img/icons/icon_512x512.fe1ca0d9703f62b945afde8e6dd15c7c.png"
   },
   {
     "url": "img/icons/icon_60x60.693a00de64dde914a40128bf738bfef0.png"
   },
   {
-    "url": "img/icons/icon_72x72.65f1dc0a4561e2ef4fd545f8c7794204.png"
+    "url": "img/icons/icon_96x96.cb037fb03dfce6eefc988195591813f6.png"
   },
   {
     "url": "img/icons/icon_76x76.9986db0d0c76bcf6f2293d756678fe2a.png"
   },
   {
-    "url": "img/icons/icon_96x96.cb037fb03dfce6eefc988195591813f6.png"
+    "url": "img/icons/icon_72x72.65f1dc0a4561e2ef4fd545f8c7794204.png"
   },
   {
     "url": "img/icons/icon_152x152.7eebc6f3b7f2c9aed5f165f6fa206cfc.png"
@@ -49,18 +58,9 @@ self.__precacheManifest = [
     "url": "img/icons/icon_128x128.1102737eb072d6838a080fd20cf226b1.png"
   },
   {
-    "url": "img/icons/icon_120x120.7eadb215beec2ed72e5eac4a51da56fb.png"
-  },
-  {
     "url": "img/icons/icon_114x114.f786f1a31738cf79fde064dd433a2416.png"
   },
   {
     "url": "img/icons/icon_1024x1024.5301d7d44558656ec63669e7525aed34.png"
-  },
-  {
-    "url": "INB-L0401/js/main-a8bcbec0785434fc9268.js"
-  },
-  {
-    "url": "INB-L0401/css/main-e4f79b6af3185b2e6706.css"
   }
 ];
